c148e33899f118e6eb7fca02c5677ade90f889e4
Stanislas Nanchen <stan@hood.ch>
2012-10-11 09:18:20 +0200

    transfer

--- src/viaz/core.clj
+++ src/viaz/core.clj
@@ -3,18 +3,33 @@
 	[:require [clojure.zip :as zip]]
 	[:require [clj-time.core :as time]]
 	[:require [clj-time.format :as format]]
+	[:require [clojure.data.zip.xml :as dzip]]
+	[:import [org.joda.time LocalDate ReadableInstant ReadablePeriod]]
 	)
 
-(def zimbra-day-formater (format/formatter "yyyyMMdd"))
+(def zimbra-base-url "http://zimbra.ergon.ch/home/nanchen/viaz.xml")
 
-(def zimbra-base-url "http://zimbra.ergon.ch/home/nanchen/viaz.xml?view=day&date=")
+(extend-protocol time/DateTimeProtocol
+  org.joda.time.LocalDate
+  (year [this] (.getYear this))
+  (month [this] (.getMonthOfYear this))
+  (day [this] (.getDayOfMonth this))
+  (day-of-week [this] (.getDayOfWeek this))
+  (hour [this] (.getHourOfDay this))
+  (minute [this] 0)
+  (sec [this] 0)
+  (milli [this] 0)
+  (after? [this #^ReadableInstant that] (.isAfter this that))
+  (before? [this #^ReadableInstant that] (.isBefore this that))
+  (plus- [this #^ReadablePeriod period] (.plus this period))
+  (minus- [this #^ReadablePeriod period] (.minus this period)))
 
 (defn absolute-day [relative]
 	(let [day (time/plus (time/now) (time/days relative))]
 		(format/unparse zimbra-day-formater day)))
 
-(defn zimbra-url [formated-day]
-	(str zimbra-base-url formated-day))
+(defn zimbra-url [start end]
+	(str zimbra-base-url "&start=" start "&end=" end))
 
 (defn relative-zimbra-zip [relative]
 	(-> relative absolute-day zimbra-url xml/parse zip/xml-zip))
@@ -22,4 +37,28 @@
 (defn absolute-zimbra-zip [absolute-day]
 	(-> absolute-day zimbra-url xml/parse zip/xml-zip))
 
-(zip/xml-zip (xml/parse "http://zimbra.ergon.ch/home/nanchen/viaz.xml?view=day&date=2012101"))
+(defn compute-duration-in-hour [start end]
+	(/ (- end start) 1000.0 60 60))
+
+(defn extract-timestamp [comp-node label]
+	(let [extracted-millis (dzip/xml1-> comp-node label (dzip/attr :u))]
+		(Long/parseLong extracted-millis)))
+
+(defn extract-appointment [comp-node]
+	(let [comp-node-attrs (-> comp-node zip/node :attrs)
+		  [project activity] (clojure.string/split (comp-node-attrs :loc) #"/")
+		  start (extract-timestamp comp-node :s)
+		  end (extract-timestamp comp-node :e)]
+		(hash-map
+			:name (comp-node-attrs :name)
+			:project project
+			:activity activity
+			:duration (compute-duration-in-hour start end))))
+
+(defn extract-comps [xml-doc]
+	(map extract-appointment (dzip/xml-> xml-doc :appt :inv :comp)))
+
+(defn extract-viaz-add [{:keys [name project activity duration]}]
+	(str "viaz-add " name project activity duration))
+
+(def ex (zip/xml-zip (xml/parse "file:///Users/stan/Downloads/viaz.xml")))
