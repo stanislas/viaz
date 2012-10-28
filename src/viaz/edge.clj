d8a2fcee86e01ff078f921cf75dc56a1784dc1b5
Stanislas Nanchen <stan@hood.ch>
2012-10-29 00:45:38 +0100

    further dev of edge.

--- src/viaz/edge.clj
+++ src/viaz/edge.clj
@@ -1,15 +1,25 @@
 (ns viaz.edge
   (:use [ring.adapter.jetty :only [run-jetty]])
   (:use [ring.util.response :only [response]])
-  (:use [net.cgrand.moustache :only [app]]))
+  (:use [net.cgrand.moustache :only [app]])
+  (:require [viaz.cal :as cal]))
 
+(defn error-with-map [error-map]
+	(constantly {:status 404
+               :body (str error-map)}))
+
+(defn name-period-handler [name period-expression]
+	(try
+		(let [period (cal/parse-time-expression period-expression)]
+			(response (str {:name name :period period})))
+	 (catch IllegalArgumentException e
+	 	(error-with-map {:exception e :message (.getMessage e) :stack (.getStackTrace e)}))))
 
 (def main-handler
   (app
     [name] (constantly {:body name})
-    [name period] (constantly {:body (str "name: " name " ;period=" period)})
-    [&]        (constantly {:status 404
-               :body "Page Not Found"})))
+    [name period] (constantly (name-period-handler name period))
+    [&]        (error-with-map {})))
 
 (defonce server (run-jetty main-handler
                            {:port 8001 :join? false}))
