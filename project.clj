4fb591318d19399e7722d54c8c1118f97ed7911c
Stanislas Nanchen <stan@hood.ch>
2015-11-27 15:48:17 +0100

    update dependencies.

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-time "0.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-time "0.11.0"]
                  [org.clojure/data.zip "0.1.1"]
-                 [ring/ring-core "1.2.1"]
-                 [ring/ring-jetty-adapter "1.2.1"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [net.cgrand/moustache "1.1.0"]
-                 [enlive "1.1.5"]
-                 [clj-http "0.7.7"]]
+                 [enlive "1.1.6"]
+                 [clj-http "2.0.0"]]
   :plugins [[lein-ring "0.8.8"]]
   :ring {:handler viaz.edge/main-handler}
   :profiles {:dev
